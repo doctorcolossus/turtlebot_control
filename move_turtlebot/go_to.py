e6f8f039f73bc05db151e013b1e8aa49655474b9
Casey Jones <casey.jones@caci.com>
2022-12-15 06:36:50 +0200

    go_to.py: replace interactive input with cmd args
    go_to.py: improve readability

--- move_turtlebot/go_to.py
+++ move_turtlebot/go_to.py
@@ -1,174 +1,208 @@
 #!/usr/bin/env python
-#!/usr/bin/env python
+
+from argparse import ArgumentParser
+from sys import argv
+
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from math import pow, atan2, sqrt
 
+parser = ArgumentParser()
 
-class MoveToPoz(Node):
+parser.add_argument('coordinates')
+parser.add_argument('-d', '--distance_tolerance', default=0.1)
+parser.add_argument('-o', '--orientation')
+parser.add_argument('-a', '--angular_tolerance', default=0.1)
+parser.add_argument('-i', '--use_i_controller', action='store_true')
+
+arguments = parser.parse_args()
+
+if arguments.coordinates.count(',') != 1:
+  raise ValueError("coordinates must be given as x,y")
+
+arguments.coordinates = [float(coordinate)
+                         for coordinate
+                         in arguments.coordinates.split(',')]
+
+class MoveTo(Node):
 
     def __init__(self):
-        # Creates a node with name 'turtlebot_controller'        
-        super().__init__('turtlebot_controller')
 
-        # Publisher which will publish to the topic '/cmd_vel'.
+        super().__init__(node_name = 'turtlebot_controller')
+
         self.velocity_publisher = self.create_publisher(Twist, '/cmd_vel', 10)
 
-        # A subscriber to the topic '/pose' with a calback to 'update_pose'
-        self.pose_subscriber = self.create_subscription(Odometry, '/odom', self.update_pose, 10)
+        self.pose_subscriber = \
+            self.create_subscription(msg_type    = Odometry,
+                                     topic       = '/odom',
+                                     callback    = self.update_pose,
+                                     qos_profile = 10)
 
-        timer_period = 0.1  # seconds
-        self.state = 0 # if movement in progress, eaquals to 1
-        self.init_theta = "y"
-        self.g_p = Odometry()
-        self.g_p.pose.pose.position.x = 0.0
-        self.g_p.pose.pose.position.y = 0.0
-        self.g_p.pose.pose.orientation.z = 0.0
-        self.distance_tolerance = 0.1
-        self.angular_tolerance = 0.1
+        TIMER_PERIOD = 0.1
+
+        self.state = "idle"
+
+        self.goal = Odometry()
+
+        self.goal.pose.pose.position.x = arguments.coordinates[0]
+        self.goal.pose.pose.position.y = arguments.coordinates[1]
+
+        if arguments.orientation:
+          self.goal.pose.pose.orientation.z = float(arguments.orientation)
+
+        self.distance_tolerance = arguments.distance_tolerance
+        self.angular_tolerance  = arguments.angular_tolerance
+
         self.I_value = 0.0
 
-        # Initialize a timer that excutes call back function in desired frequency
-        self.timer = self.create_timer(timer_period, self.move2goal)
+        self.timer = \
+            self.create_timer(timer_period_sec = TIMER_PERIOD,
+                              callback         = self.move_to_position)
 
         self.pose = Odometry()
         
     def update_pose(self, data):        
-        #Callback function which is called when a new message of type Odometry is
-        #received by the subscriber.
+        """called when new Odometry message Odometry received by subscriber"""
+
         self.pose = data
         self.pose.pose.pose.position.x = round(self.pose.pose.pose.position.x, 4)
         self.pose.pose.pose.position.y = round(self.pose.pose.pose.position.y, 4)
 
     def euclidean_distance(self, goal_pose):
-        #Distance between current pose and the goal.
+        """returns distance between current pose and goal"""
+
         return float(sqrt(pow((goal_pose.pose.pose.position.x - self.pose.pose.pose.position.x), 2) +
                     pow((goal_pose.pose.pose.position.y - self.pose.pose.pose.position.y), 2)))
 
-    def linear_vel(self, goal_pose, constant_P=0.2, constant_I = 0.01):
-        #Proportional controler for lin. velocity controll
-        #velocity = constant * self.euclidean_distance(goal_pose)
-        velocity = self.PI_controller(self.euclidean_distance(goal_pose), constant_P, constant_I)
-        if velocity > 0.4:
-            velocity = 0.4
-        elif velocity < -0.4:
-            velocity = -0.4
+    def linear_velocity(self, goal_pose, constant_P = 0.2, constant_I = 0.01):
+        """proportional controller for linear velocity control"""
+
+        velocity = self.PI_controller(self.euclidean_distance(goal_pose),
+                                      constant_P,
+                                      constant_I)
+
+        velocity = min(velocity,  0.4)
+        velocity = max(velocity, -0.4)
+
         return velocity
 
     def steering_angle(self, goal_pose):
-        #Angle of the path from robot location to destination.
-        return atan2(goal_pose.pose.pose.position.y - self.pose.pose.pose.position.y, goal_pose.pose.pose.position.x - self.pose.pose.pose.position.x)
+        """returns angle of path from robot location to destination"""
+
+        return atan2((goal_pose.pose.pose.position.y
+                      - self.pose.pose.pose.position.y),
+                     (goal_pose.pose.pose.position.x
+                      - self.pose.pose.pose.position.x))
 
     def angular_vel(self, goal_pose, constant_P=1, constant_I=0.1):
-        #Proportional controler to align robot to the direction of the destination.
-        #omega = constant * (self.steering_angle(goal_pose) - self.pose.pose.pose.orientation.z)
-        omega = self.PI_controller((self.steering_angle(goal_pose) - self.pose.pose.pose.orientation.z), constant_P, constant_I)
-        if omega > 0.5:
-            omega = 0.5
-        elif omega < -0.5:
-            omega = -0.5
+        """proportional controller to orient robot toward destination"""
+
+        omega = \
+            self.PI_controller(delta = (self.steering_angle(goal_pose)
+                                        - self.pose.pose.pose.orientation.z),
+                               gain_P = constant_P,
+                               gain_I = constant_I)
+
+        omega = min(omega,  0.5)
+        omega = max(omega, -0.5)
+
         return omega
 
     def angular_difference(self, goal_pose):
-        delta = self.pose.pose.pose.orientation.z - goal_pose.pose.pose.orientation.z
-        return delta
 
-    ############contoler_stuff#################
+        delta = (self.pose.pose.pose.orientation.z
+                 - goal_pose.pose.pose.orientation.z)
+
+        return delta
 
     def P_controller(self, delta, gain):
         P_value = gain * delta
         return P_value
 
-    def PI_controller(self, Delta, gain_P, gain_I):
-        self.I_value = self.I_value + Delta * gain_I
-        if self.I_value > 0.3:
-            self.I_value = 0.3
-        elif self.I_value < -0.3:
-            self.I_value = -0.3
+    def PI_controller(self, delta, gain_P, gain_I):
 
-        use_I_controler = True #if set to true PI controler wil be used insted of just P
+        self.I_value = self.I_value + delta * gain_I
 
-        if use_I_controler:
-            return self.I_value + self.P_controller(Delta, gain_P)
+        self.I_value = min(self.I_value,  0.3)
+        self.I_value = max(self.I_value, -0.3)
+
+        if arguments.use_i_controller:
+            return self.I_value + self.P_controller(delta, gain_P)
         else:
-            return self.P_controller(Delta, gain_P)
+            return self.P_controller(delta, gain_P)
 
-    ##########################################
+    def move_to_position(self):
 
-    def move2goal(self):
-        #Moves the turtle to the goal        
         goal_pose = Odometry()
 
-        if self.state == 0:
-            # Get the input from the user.
-            self.g_p.pose.pose.position.x = float(input("Set your x goal: "))
-            self.g_p.pose.pose.position.y = float(input("Set your y goal: "))
-            self.init_theta = input("Is theta a requirement y/n?: ")
-            if self.init_theta == "y":
-                self.g_p.pose.pose.orientation.z = float(input("Set your theta goal: "))
-                self.angular_tolerance = float(input("Set your angular tolerance: "))
+        if self.state == "idle":
 
-            # Insert a number slightly greater than 0 (e.g. 0.1).
-            self.distance_tolerance = float(input("Set your tolerance: "))
-            self.state = 1
+            if arguments.orientation:
+                self.goal.pose.pose.orientation.z = arguments.orientation
+                self.angular_tolerance = arguments.angular_tolerance
 
-        goal_pose.pose.pose.position.x = self.g_p.pose.pose.position.x
-        goal_pose.pose.pose.position.y = self.g_p.pose.pose.position.y
-        goal_pose.pose.pose.orientation.z = self.g_p.pose.pose.orientation.z
+            self.distance_tolerance = arguments.distance_tolerance
 
-        vel_msg = Twist()
+            self.state = "translation"
 
-        if self.state == 1:
+        goal_pose.pose.pose.position.x    = self.goal.pose.pose.position.x
+        goal_pose.pose.pose.position.y    = self.goal.pose.pose.position.y
+        goal_pose.pose.pose.orientation.z = self.goal.pose.pose.orientation.z
+
+        message = Twist()
+
+        if self.state == "translation":
+
             if self.euclidean_distance(goal_pose) >= self.distance_tolerance:               
-                # Linear velocity in the x-axis.
-                vel_msg.linear.x = self.linear_vel(goal_pose)
-                vel_msg.linear.y = 0.0
-                vel_msg.linear.z = 0.0
 
-                # Angular velocity in the z-axis.
-                vel_msg.angular.x = 0.0
-                vel_msg.angular.y = 0.0
-                vel_msg.angular.z = self.angular_vel(goal_pose)
+                message.linear.x = self.linear_velocity(goal_pose)
+                message.linear.y = 0.0
+                message.linear.z = 0.0
 
-                # Publishing our vel_msg
-                self.velocity_publisher.publish(vel_msg)
+                message.angular.x = 0.0
+                message.angular.y = 0.0
+                message.angular.z = self.angular_vel(goal_pose)
 
-                # Publish at the desired rate.
-                #self.rate.sleep()
-            else:
-                # Stopping our robot after destination is reached.
-                vel_msg.linear.x = 0.0
-                vel_msg.angular.z = 0.0
-                self.velocity_publisher.publish(vel_msg)
-                if self.init_theta == "y":
-                    self.state = 2
+                self.velocity_publisher.publish(message)
+
+            else: # stop after destination reached
+
+                message.linear.x = 0.0
+                message.angular.z = 0.0
+                self.velocity_publisher.publish(message)
+
+                if arguments.orientation:
+                    self.state = "rotation"
                 else:
-                    self.state = 0
-        if self.state == 2:
-            if self.angular_difference(goal_pose) > self.angular_tolerance or self.angular_difference(goal_pose) < ((-1)*self.angular_tolerance):
-               vel_msg.angular.z = (self.angular_difference(goal_pose) * 0.5)
-               self.velocity_publisher.publish(vel_msg)
+                    self.state = "idle"
+
+        if self.state == "rotation":
+
+            if (   (self.angular_difference(goal_pose)
+                    > self.angular_tolerance)
+                or (self.angular_difference(goal_pose)
+                    < -1 * self.angular_tolerance)):
+
+               message.angular.z = 0.5 * self.angular_difference(goal_pose)
+
+               self.velocity_publisher.publish(message)
+
             else:
-                vel_msg.angular.z = 0.0
-                self.velocity_publisher.publish(vel_msg)
-                self.state = 0
+                message.angular.z = 0.0
+                self.velocity_publisher.publish(message)
+                self.state = "idle"
 
 def main(args=None):
+
     rclpy.init(args=args)
 
     try:
-        x = MoveToPoz()
-        rclpy.spin(x)
+        rclpy.spin(MoveTo())
         
     except KeyboardInterrupt:
-    	# execute shutdown function
-    	#move_bobot.stop_turtlebot()
-    	# clear the node
-    	#move_bobot.destroy_node()
-    	rclpy.shutdown()
+        rclpy.shutdown()
     
-     	
 if __name__ == '__main__':
     main()
